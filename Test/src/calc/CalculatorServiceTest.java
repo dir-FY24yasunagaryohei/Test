aa19e668eb55a6d9907c3452d4ed662998b73442
ryrydirbato <ryohei.yasunaga.d819@dirbato.co.jp>
2024-04-12 10:34:24 +0900

    練習

---
+++ Test/src/calc/CalculatorServiceTest.java
@@ -0,0 +1,32 @@
+package calc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorServiceTest {
+    
+    private Calculator calculator;
+    private CalculatorService service;
+    
+    @BeforeEach
+    public void setup() {
+        //テスト前にCalculatorとCalculatorServiceをセットアップ
+        calculator = new Calculator();
+        service = new CalculatorService(calculator);
+    }
+
+    @Test
+    public void testAddAndReport(){
+        //CalculatorのaddメソッドとCalculatorServiceのaddAndReportメソッドが正しく連携するかテスト
+        assertEquals("Result: 5", service.addAndReport(2, 3));
+    }
+    
+    @Test
+    public void testSubtractAndReport() {
+        //CalculatorのsubtractメソッドとCalculatorSeviceのsubtractAndReportメソッドが正しく連携するかテスト
+        assertEquals("Result: 1", service.subtractAndReport(3, 2));
+    }
+
+}
